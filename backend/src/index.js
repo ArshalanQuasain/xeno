dc53aa39c97b5de24096e28734840e738a3b2b9e
ArshalanQuasain <arshalanquasain.01@gmail.com>
2024-11-23 21:26:23 +0530

    listen

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,19 @@
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import app from "./app.js";
 
 dotenv.config({
-    path: './.env',
+    path: './.env'
 });
 
-connectDB().catch((err) => {
-    console.error("MONGODB CONNECTION FAILED!", err);
-});
+export default app ;
 
-export default app;
+connectDB().
+    then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port: ${process.env.PORT || 8000}`);
+        });
+    }).catch((err) => {
+        console.error("MONGODB CONNECTION FAILED!", err);
+    });
+
