3eccf016b053f941727119ed35769a0556cae86d
ArshalanQuasain <arshalanquasain.01@gmail.com>
2024-11-23 21:08:37 +0530

    versel configure

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,13 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import app from "./app.js";
 
 dotenv.config({
-    path: './.env'
+    path: './.env',
 });
 
-export default app ;
+connectDB().catch((err) => {
+    console.error("MONGODB CONNECTION FAILED!", err);
+});
 
-connectDB().
-    then(() => {
-        app.listen(process.env.PORT || 8000, () => {
-            console.log(`Server is running on port: ${process.env.PORT || 8000}`);
-        });
-    }).catch((err) => {
-        console.error("MONGODB CONNECTION FAILED!", err);
-    });
-
+export default app;
